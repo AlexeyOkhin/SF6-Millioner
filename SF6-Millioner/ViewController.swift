d6a2ba240d09e1615df73e71ff85623130647a83
AlexeyOkhin <k-i-ll-e-r@list.ru>
2023-02-05 09:42:26 +0500

    Initial Commit

---
+++ SF6-Millioner/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SF6-Millioner
+//
+//  Created by Djinsolobzik on 05.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
