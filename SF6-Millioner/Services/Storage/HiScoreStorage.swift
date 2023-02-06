18ce7c94248fc607885187dccf2d2d8519d39f34
AlexeyOkhin <k-i-ll-e-r@list.ru>
2023-02-06 20:41:03 +0500

    add hiScore storage

---
+++ SF6-Millioner/Services/Storage/HiScoreStorage.swift
@@ -0,0 +1,47 @@
+//
+//  HiScoreStorage.swift
+//  SF6-Millioner
+//
+//  Created by Djinsolobzik on 06.02.2023.
+//
+
+import Foundation
+
+struct HiScoreStorage {
+
+    //MARK: - Nested Types
+
+    private enum Error: Swift.Error {
+        case storagePlistWasNotFound
+    }
+
+    //MARK: - Private Properties
+
+    private let storage: UserDefaults
+    private let keyStorage = "hiScore"
+    private let suiteName = "com.HiScoreStorage"
+
+    //MARK: -  Initialization
+
+    init() throws {
+        guard let storage = UserDefaults(suiteName: suiteName) else {
+            throw Error.storagePlistWasNotFound
+        }
+        self.storage = storage
+    }
+
+    //MARK: - Internal Methods
+
+    func saveHiScore(by name: String, new hiScore: Int) {
+        let result = "\(name) -- \(hiScore)"
+        storage.set(result, forKey: keyStorage)
+    }
+
+    func getHiScore() -> String? {
+        return storage.string(forKey: keyStorage)
+    }
+
+    func resetViewCounterStorage() {
+        storage.removeSuite(named: suiteName)
+    }
+}
