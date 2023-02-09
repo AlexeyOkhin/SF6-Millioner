dc63caa10dbaa4efebf6f5c5dd9b00e18fbb399a
AlexeyOkhin <k-i-ll-e-r@list.ru>
2023-02-09 07:57:51 +0500

    add make for Hiscore

---
+++ SF6-Millioner/MainFlow/HiScore/HiScoreViewController.swift
@@ -0,0 +1,18 @@
+//
+//  HiScoreViewController.swift
+//  SF6-Millioner
+//
+//  Created by Djinsolobzik on 09.02.2023.
+//
+
+import UIKit
+
+class HiScoreViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+    }
+
+}
