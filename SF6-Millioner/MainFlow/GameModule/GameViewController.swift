eed7a9c254a93ad808df50a543362b4e1c7d682a
ohihateit <whoamins33@gmail.com>
2023-02-12 01:24:07 +0300

    Finish the game if timer is over

--- SF6-Millioner/MainFlow/GameModule/GameViewController.swift
+++ SF6-Millioner/MainFlow/GameModule/GameViewController.swift
@@ -340,6 +340,14 @@ class GameViewController: UIViewController {
         } else {
             timer.invalidate()
             
+            let finishVC = FinishViewController(failAttempt: game.level, isWin: game.isWin, money: game.currentSum)
+            
+            progressBar.progress = 0.0
+            timer.invalidate()
+            timerSound.stop()
+            secondsPassed = 0
+            
+            self.navigationController?.pushViewController(finishVC, animated: true)
         }
     }
     
